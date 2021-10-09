f649c6053769dec3151321bccf181daadae70d49
Ldotexe <90475971+Ldotexe@users.noreply.github.com>
2021-10-09 06:10:05 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <stdio.h>
 #include "squaresolution.h"
+#include "sq.solution.test.h"
+#include "math.h"
 //#include "sq.solution.test.h"
 
-void solving_equations (int *n, double *x1, double *x2, double *x3);
-void print_result_eq (int n, double x1, double x2, double x3);
+sq_solution_error solving_equations (int *n, double *x1, double *x2, double *x3);
+sq_solution_error print_result_eq (int n, double x1, double x2, double x3);
 
 int main()
 {
-    double x1 = NULL, x2 = NULL, x3 = NULL;
-    int n = NULL;
-    test_square_solution();
+    double x1 = NAN, x2 = NAN, x3 = NAN;
+    int n = NAN;
     solving_equations(&n, &x1, &x2, &x3);
     return 0;
 }
 
-void solving_equations (int *n, double *x1, double *x2, double *x3){
+sq_solution_error solving_equations (int *n, double *x1, double *x2, double *x3){
+    if (n == nullptr || x1 == nullptr || x2 == nullptr || x3 == nullptr){
+        return SQ_SOLUTION_NO_POINTER;
+    }
     int mode = NULL;
     while(mode + 1){
-        printf("Please, enter mode of working in range from 1 to 3 or end program with -1\n");
+        printf("Please, enter mode of working in range from 1 to 3 or end program with -1\n"
+               "Or you can test square solution program with 0 mode\n\n");
         if (!scanf("%d", &mode)){
             break;
         }
@@ -27,50 +32,73 @@ void solving_equations (int *n, double *x1, double *x2, double *x3){
             printf("Thank you for using my program!\n"
                    "Good bye!\n");
             break;
+        case 0:
+            printf("Tests passed %s\n\n", test_cube_solution() == 0 ? "Yes" : "No") ;
+            break;
         case 1:{
-            double a = NULL, b = NULL;
-            scanf("%lg %lg", &a, &b);
-            linear_solution(a, b, x1, n);
-            print_result_eq (*n, *x1, *x2, *x3);
+            double a = NAN, b = NAN;
+            if (scanf("%lg %lg", &a, &b) && !isnan(a) && !isnan(b)){
+                linear_solution(a, b, x1, n);
+                print_result_eq (*n, *x1, *x2, *x3);
+            }
+            else{
+                return SQ_SOLUTION_OK;
+            }
             break;
         }
         case 2:{
-            double a = 0, b = NULL, c = NULL;
-            scanf("%lg %lg %lg", &a, &b, &c);
-            square_solution(a, b, c, x1, x2, n);
-            print_result_eq (*n, *x1, *x2, *x3);
+            double a = NAN, b = NAN, c = NAN;
+            if (scanf("%lg %lg %lg", &a, &b, &c) && !isnan(a) && !isnan(b) && !isnan(c)){
+                square_solution(a, b, c, x1, x2, n);
+                print_result_eq (*n, *x1, *x2, *x3);
+            }
+            else{
+                return SQ_SOLUTION_OK;
+            }
             break;
         }
         case 3:{
-            printf("Oops! We can't solve equations with 3 roots yet\n");
+            double a = NAN, b = NAN, c = NAN, d = NAN;
+            if(scanf("%lg %lg %lg %lg", &a, &b, &c, &d) && !isnan(a) && !isnan(b) && !isnan(c) && !isnan(d)){
+                cubic_solution(a, b, c, d, x1, x2, x3, n);
+                print_result_eq (*n, *x1, *x2, *x3);
+            }
+            else{
+                return SQ_SOLUTION_OK;
+            }
             break;
         }
         default:
             printf("Oops! Wrong mode!\n"
-                   "Please, try again, mode can be in range from 1 to 3\n");
+                   "Please, try again, mode can be in range from 1 to 3\n\n");
         }
     }
+    return SQ_SOLUTION_OK;
 }
 
 
-void print_result_eq (int n, double x1, double x2, double x3){
+sq_solution_error print_result_eq (int n, double x1, double x2, double x3){
+    if (isnan(n)){
+        return SQ_SOLUTION_NAN;
+    }
     switch(n){
         case -1:
             printf("Infinite number of roots\n");
             break;
         case 0:
-            printf("Number of roots: %d\n", n);
+            printf("Number of real roots: %d\n", n);
             break;
         case 1:
-            printf("Number of roots: %d\n%g\n", n, x1);
+            printf("Number of real roots: %d\n%g\n", n, x1);
             break;
         case 2:
-            printf("Number of roots: %d\n%g %g\n", n, x1, x2);
+            printf("Number of real roots: %d\n%g %g\n", n, x1, x2);
             break;
         case 3:
-            printf("Number of roots: %d\n%g %g %g\n", n, x1, x2, x3);
+            printf("Number of real roots: %d\n%g %g %g\n", n, x1, x2, x3);
             break;
         default:
-            printf("Number of roots: Oops! Please, make sure that number of roots is in range from 0 to 3\n");
+            printf("Oops! Please, make sure that number of real roots is in range from 0 to 3\n");
     }
+    return SQ_SOLUTION_OK;
 }
--- squaresolution.cpp
+++ squaresolution.cpp
@@ -2,21 +2,33 @@
 #include <stdio.h>
 #include "squaresolution.h"
 
+const double pi = 3.1416;//3.1415926535897932384626433832795028;
+
 const float consr = 0.0001;
 
-int equality (double a, double b)
-{
-    if (fabs(a-b) <= consr)
-    {
+const char* sq_solution_value[] = {
+    "SQ_SOLUTION_OK",
+    "SQ_SOLUTION_NO_POINTER",
+    "SQ_SOLUTION_NAN"
+};
+
+int equality (double a, double b, double epsilon){
+    if (fabs(a-b) <= epsilon){
         return 1;
     }
-    else
-    {
+    else{
         return 0;
     }
 }
 
-void linear_solution (double b, double c, double *x1, int *n){
+sq_solution_error linear_solution (double b, double c, double *x1, int *n){
+    // a * x + b = 0
+    if (x1 == nullptr || n == nullptr){
+        return SQ_SOLUTION_NO_POINTER;
+    }
+    if (isnan(b) || isnan(c)){
+        return SQ_SOLUTION_NAN;
+    }
     if (!equality(b, 0))
     {
         *x1 = -c/b;
@@ -24,15 +36,23 @@ void linear_solution (double b, double c, double *x1, int *n){
     }
     else
     {
-        if(!equality(c, 0))
+        if (!equality(c, 0))
             *n = 0;
         else
             *n = INF;
     }
+    return SQ_SOLUTION_OK;
 }
 
-void square_solution (double a, double b, double c, double *x1, double *x2, int *n)
+sq_solution_error square_solution (double a, double b, double c, double *x1, double *x2, int *n)
 {
+    // a * x^2 + b * x + c = 0
+    if (x1 == nullptr || x2 == nullptr || n == nullptr){
+        return SQ_SOLUTION_NO_POINTER;
+    }
+    if (isnan(a) || isnan(b) || isnan(c)){
+        return SQ_SOLUTION_NAN;
+    }
     if (equality(a, 0))
     {
         linear_solution(b, c, x1, n);
@@ -58,4 +78,67 @@ void square_solution (double a, double b, double c, double *x1, double *x2, int 
             }
         }
     }
+    return SQ_SOLUTION_OK;
+}
+
+sq_solution_error cubic_solution (double a, double b, double c, double d, double *x1, double *x2, double *x3, int *n){
+    // a * x^3 + b * x^2 + c * x + d = 0
+    if (x1 == nullptr || x2 == nullptr || x3 == nullptr || n == nullptr){
+        return SQ_SOLUTION_NO_POINTER;
+    }
+    if (isnan(a) || isnan(b)  || isnan(c) || isnan(d)){
+        return SQ_SOLUTION_NAN;
+    }
+    if (equality(a, 0)){
+        square_solution(b, c, d, x1, x2, n);
+    }
+    else{
+        // x = y - b / 3 * a
+        double p = (3 * a * c - b * b) / (3 * a * a);
+        double q = (2 * b * b * b - 9 * a * b * c + 27 * a * a * d) / (27 * a * a * a);
+        // y^3 + p * y + q = 0
+        double s = (q * q / 4) + (p * p * p / 27); // s - Discriminant
+        if (s < 0){
+            double F = pi / 2;
+            if (q < 0){
+                F = atan(sqrt(-s) / (- q / 2));
+            }
+            if (q > 0){
+                F = atan(sqrt(-s) / (- q / 2)) + pi;
+            }
+            *n = 3;
+            *x1 = 2 * sqrt(-p / 3) * cos(F / 3) - b / (3 * a);
+            *x2 = 2 * sqrt(-p / 3) * cos(F / 3 + 2 * pi / 3) - b / (3 * a);
+            *x3 = 2 * sqrt(-p / 3) * cos(F / 3 + 4 * pi / 3) - b / (3 * a);
+            return SQ_SOLUTION_OK;
+        }
+        if (equality(s, 0)){
+            *n = 2;
+            if (- q / 2 < 0){
+                *x1 = - 2 * pow(q / 2, 0.333) - b / (3 * a);
+                *x2 = pow(q / 2, 0.333) - b / (3 * a);
+            }
+            else{
+                *x1 = 2 * pow(-q / 2, 0.333) - b / (3 * a);
+                *x2 = - pow(-q / 2, 0.333) - b / (3 * a);
+            }
+            return SQ_SOLUTION_OK;
+        }
+        if (s > 0){
+            *n = 1;
+            if (-q / 2 - sqrt(s) > 0){
+                *x1 = pow((-q / 2) + sqrt(s), 0.333) + pow((-q / 2) - sqrt(s), 0.333) - b / (3 * a);
+            }
+            else{
+                if ((-q / 2) + sqrt(s) < 0){
+                    *x1 = - pow((q / 2) - sqrt(s), 0.333) - pow((q / 2) + sqrt(s), 0.333) - b / (3 * a);
+                }
+                else{
+                    *x1 = pow((-q / 2) + sqrt(s), 0.333) - pow((q / 2) + sqrt(s), 0.333) - b / (3 * a);
+                }
+            }
+            return SQ_SOLUTION_OK;
+        }
+    }
+    return SQ_SOLUTION_OK;
 }
